33684b7ad021ae2afa260ac334bd053811b43d8b
Shengfeng Chen <mike.chen1632@gmail.com>
2018-03-20 14:25:41 -0400

    Update lattice_paths.py

--- lattice_paths.py
+++ lattice_paths.py
@@ -2,16 +2,17 @@
 Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, 
 there are exactly 6 routes to the bottom right corner. How many paths exists for 20x20 grid?
 '''
-a = 20
-s = []
-for i in range(a):
-  s.append(i+2)
+grid = 20
+route = []
 
-for j in range(a-1):
-    for i in range(a):
+for i in range(grid):
+  route.append(i+2)
+
+for j in range(grid-1):
+    for i in range(grid):
         if i == 0:
-            s[i] = s[i] + 1
+            route[i] = route[i] + 1
         else:
-            s[i] = s[i] + s[i-1]
+            route[i] = route[i] + route[i-1]
 
-print(s[a-1])
+print(route[grid-1])
